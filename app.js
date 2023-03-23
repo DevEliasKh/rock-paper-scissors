00d2b8a0b7cc43d01c4c054c1357f50d08c0b2bf
Eliaskh77 <20316165+Eliaskh77@users.noreply.github.com>
2023-03-23 12:12:01 +0330

    add ui design

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-function playRound(playerSelction, computerSelction) {
-	let choice = ['ROCK', 'PAPER', 'SCISSORS'];
-	let computerSelection = getComputerChoice();
+const buttons = document.querySelectorAll('.buttons button');
+const human = document.querySelector('.human');
+const computer = document.querySelector('.computer');
+const humanScore = document.querySelector('.human_score .score');
+const computerScore = document.querySelector('.computer_score .score');
+const computerChoice = document.querySelector('.computer_choice');
+const humanChoice = document.querySelector('.human_choice');
+const final = document.querySelector('.final');
+const message = document.querySelector('.message');
+const playAgian = document.querySelector('.play_again');
 
+let playerSelection;
+
+buttons.forEach((button) => {
+	const choice = button.className;
+	button.addEventListener('click', () => {
+		playerSelection = choice;
+		playRound();
+	});
+});
+
+let computerSelection;
+
+function playRound() {
+	let playersChoice = ['ROCK', 'PAPER', 'SCISSORS'];
+	computerSelection = getComputerChoice();
 	function getComputerChoice() {
 		let randomNum = Math.floor(Math.random() * 3);
-		let computerChoice = choice[randomNum];
-		return computerChoice;
+		let computerChoices = playersChoice[randomNum];
+		return computerChoices;
 	}
-	// console.log(computerSelection);
-
-	let playerSelection = prompt(
-		'Choose between Rock, Paper and Secissors'
-	).toLocaleUpperCase();
-	// console.log(playerSelection);
-
 	if (playerSelection === computerSelection) {
 		console.log('Tie game');
 	} else if (
@@ -21,6 +36,7 @@ function playRound(playerSelction, computerSelction) {
 		(computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
 		(computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
 	) {
+		updateScore('computer');
 		console.log(
 			`oh ${computerSelection} beats ${playerSelection} you lose this time!, try another round`
 		);
@@ -29,15 +45,81 @@ function playRound(playerSelction, computerSelction) {
 		(playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
 		(playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
 	) {
+		updateScore('human');
 		console.log(
 			`YES ${playerSelection} beats ${computerSelection} you win this time!, try another round`
 		);
 	}
-}
-
-function game() {
-	for (let i = 0; i < 5; i++) {
-		playRound();
+	updateChoice();
+	if (computerScore.innerText === '5') {
+		final.style.visibility = 'visible';
+		message.innerText = `The Winner Is Computer`;
+	} else if (humanScore.innerText === '5') {
+		final.style.visibility = 'visible';
+		message.innerText = `The Winner Is Human`;
 	}
 }
-game();
+
+function updateChoice() {
+	humanChoice.innerText = playerSelection;
+	computerChoice.innerText = computerSelection;
+}
+function updateScore(winner) {
+	if (winner === 'computer') {
+		computerScore.innerText++;
+	} else if (winner === 'human') {
+		humanScore.innerText++;
+	}
+}
+
+playAgian.addEventListener('click', () => {
+	computerScore.innerText = '0';
+	humanScore.innerText = '0';
+	final.style.visibility = 'hidden';
+	humanChoice.innerText = '';
+	computerChoice.innerText = '';
+});
+
+// function playRound(playerSelction, computerSelction) {
+// 	let choice = ['ROCK', 'PAPER', 'SCISSORS'];
+// 	let computerSelection = getComputerChoice();
+
+// 	function getComputerChoice() {
+// 		let randomNum = Math.floor(Math.random() * 3);
+// 		let computerChoice = choice[randomNum];
+// 		return computerChoice;
+// 	}
+// 	// console.log(computerSelection);
+
+// 	let playerSelection = prompt(
+// 		'Choose between Rock, Paper and Secissors'
+// 	).toLocaleUpperCase();
+// 	// console.log(playerSelection);
+
+// 	if (playerSelection === computerSelection) {
+// 		console.log('Tie game');
+// 	} else if (
+// 		(computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+// 		(computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
+// 		(computerSelection === 'SCISSORS' && playerSelection === 'PAPER')
+// 	) {
+// 		console.log(
+// 			`oh ${computerSelection} beats ${playerSelection} you lose this time!, try another round`
+// 		);
+// 	} else if (
+// 		(playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+// 		(playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+// 		(playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+// 	) {
+// 		console.log(
+// 			`YES ${playerSelection} beats ${computerSelection} you win this time!, try another round`
+// 		);
+// 	}
+// }
+
+// function game() {
+// 	for (let i = 0; i < 5; i++) {
+// 		playRound();
+// 	}
+// }
+// game();
